0113961fb45d4f2c127d621787ada6467ccffa38
AdrMontenegro <adrmontenegro97@gmail.com>
2025-12-05 03:08:38 +0100

    project init

---
+++ src/main/java/com/adrdev/streaming/StreamingBotServiceApplication.java
@@ -0,0 +1,13 @@
+package com.adrdev.streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingBotServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingBotServiceApplication.class, args);
+	}
+
+}
